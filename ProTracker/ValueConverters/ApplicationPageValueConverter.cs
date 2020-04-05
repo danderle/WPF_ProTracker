b243d296cf19608d9d3ba4385b3f3462d9401a60
danderle <danderle86@gmail.com>
2020-04-05 07:23:39 +0200

    converts an enum data model to an application page

---
+++ ProTracker/ValueConverters/ApplicationPageValueConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Text;
+
+namespace ProTracker
+{
+    /// <summary>
+    /// Converts the <see cref="ApplicationPage"/> to an actual view/page
+    /// </summary>
+    public class ApplicationPageValueConverter : BaseValueConverter<ApplicationPageValueConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // Find the appropriate page
+            switch ((ApplicationPage)value)
+            {
+                case ApplicationPage.START:
+                    //return new StartPage();
+
+                default:
+                    Debugger.Break();
+                    return null;
+            }
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
