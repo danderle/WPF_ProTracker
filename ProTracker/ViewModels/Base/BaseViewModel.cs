78505d06ad8cfaf458a4d06ccee442eaf7768b34
danderle <danderle86@gmail.com>
2020-04-05 07:20:26 +0200

    Base functionality for all the viewmodels

---
+++ ProTracker/ViewModels/Base/BaseViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace ProTracker
+{
+    /// <summary>
+    /// A base view model that fires Property Changed events as needed
+    /// </summary>
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// The event that is fired when any child property changes its value
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged = (sender, e) => { };
+
+        /// <summary>
+        /// Call this to fire a <see cref="PropertyChanged"/> event
+        /// </summary>
+        /// <param name="name"></param>
+        public void OnPropertyChanged(string name)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
