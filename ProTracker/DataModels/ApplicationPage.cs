197b9f1a56fc4416d19b09b283017b195b743aff
danderle <danderle86@gmail.com>
2020-04-05 07:19:49 +0200

    Data model for the pages

---
+++ ProTracker/DataModels/ApplicationPage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProTracker
+{
+    /// <summary>
+    /// Represents a page of the application
+    /// </summary>
+    public enum ApplicationPage
+    {
+        /// <summary>
+        /// The start page
+        /// </summary>
+        START = 0,
+    }
+}
