7c5652f285becd6ac0fe024438c35bae34bb7255
danderle <danderle86@gmail.com>
2020-04-05 07:22:44 +0200

    base class for all pages

---
+++ ProTracker/Pages/BasePage.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls;
+
+namespace ProTracker
+{
+    /// <summary>
+    /// A base page for all pages to gain base functionality
+    /// </summary>
+    public class BasePage<VM> : Page
+        where VM : BaseViewModel, new()
+    {
+        #region Private Member
+
+        /// <summary>
+        /// The View Model associated with this page
+        /// </summary>
+        private VM mViewModel;
+
+        #endregion
+
+        #region Public Properties
+
+        /// <summary>
+        /// The View Model associated with this page
+        /// </summary>
+        public VM ViewModel
+        {
+            get { return mViewModel; }
+            set
+            {
+                // If nothing has changed, return
+                if (mViewModel == value)
+                    return;
+
+                // Update the value
+                mViewModel = value;
+
+                // Set the data context for this page
+                this.DataContext = mViewModel;
+            }
+        }
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public BasePage()
+        {
+            // Create a default view model
+            this.ViewModel = new VM();
+        }
+
+        #endregion
+    }
+}
