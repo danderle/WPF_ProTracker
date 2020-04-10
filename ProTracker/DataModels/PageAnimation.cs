43772482f0b7f522e1447f9702ec5a50ac9874ec
danderle <danderle86@gmail.com>
2020-04-10 08:01:30 +0200

    DataModel for the different page animations

---
+++ ProTracker/DataModels/PageAnimation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProTracker
+{
+    /// <summary>
+    /// Page animation options
+    /// </summary>
+    public enum PageAnimation
+    {
+        /// <summary>
+        /// Do nothing
+        /// </summary>
+        None = 0,
+
+        /// <summary>
+        /// Slide in from the right and also fade in
+        /// </summary>
+        SlideAndFadeInFromRight,
+
+        /// <summary>
+        /// Slide out to the left and also fade out
+        /// </summary>
+        SlideAndFadeOutToLeft,
+    }
+}
