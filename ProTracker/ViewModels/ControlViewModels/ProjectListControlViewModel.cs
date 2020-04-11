0420581e5d0715dc365dd546c3abf70dbc3cba9d
danderle <danderle86@gmail.com>
2020-04-11 18:04:21 +0200

    The view model for the ProjectListControl

---
+++ ProTracker/ViewModels/ControlViewModels/ProjectListControlViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace ProTracker
+{
+    /// <summary>
+    /// A view model for each the project list creating the overview
+    /// </summary>
+    public class ProjectListControlViewModel : BaseViewModel
+    {
+        /// <summary>
+        /// The project list items for the project list
+        /// </summary>
+        public List<ProjectItemControlViewModel> Items { get; set; }
+    }
+}
