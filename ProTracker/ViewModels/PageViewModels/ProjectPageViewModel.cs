4f9c77ef0c31c1b96f57dfa9022cb55f1aa0b304
danderle <danderle86@gmail.com>
2020-04-11 18:03:50 +0200

    The view model for the ProjectPage

---
+++ ProTracker/ViewModels/PageViewModels/ProjectPageViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Input;
+
+namespace ProTracker
+{
+    /// <summary>
+    /// The view model for the <see cref="ProjectPage.xaml"/>
+    /// </summary>
+    public class ProjectPageViewModel : BaseViewModel
+    {
+        #region Private Members
+
+
+
+        #endregion
+
+        #region Public Properties
+
+
+
+        #endregion
+
+        #region Commands
+
+        /// <summary>
+        /// Command to create a new project
+        /// </summary>
+        public ICommand NewProjectCommand { get; set; }
+
+        /// <summary>
+        /// Command to continue a previous project
+        /// </summary>
+        public ICommand ContinueProjectCommand { get; set; }
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public ProjectPageViewModel()
+        {
+            //Create commands
+
+        }
+
+
+        #endregion
+    }
+}
