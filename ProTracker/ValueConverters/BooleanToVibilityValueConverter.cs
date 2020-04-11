a91dc69ccf95e3308ca06b4a6aee1117eb055a73
danderle <danderle86@gmail.com>
2020-04-11 18:00:17 +0200

    changes a boolean to visibilty

---
+++ ProTracker/ValueConverters/BooleanToVibilityValueConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Text;
+using System.Windows;
+
+namespace ProTracker
+{
+    /// <summary>
+    /// Converts the <see cref="Boolean"/> to an actual view/page
+    /// </summary>
+    public class BooleanToVibilityValueConverter : BaseValueConverter<BooleanToVibilityValueConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (bool)value ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
