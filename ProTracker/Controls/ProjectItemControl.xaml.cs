8ae1afeee80c10ee9ee9704a34067e3b1f976775
danderle <danderle86@gmail.com>
2020-04-11 17:53:11 +0200

    The single project item displayed in the side menu control

---
+++ ProTracker/Controls/ProjectItemControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace ProTracker
+{
+    /// <summary>
+    /// Interaction logic for ProjectItemControl.xaml
+    /// </summary>
+    public partial class ProjectItemControl : UserControl
+    {
+        public ProjectItemControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
