5db4ae9f69a4bf584c07a1491f8197ed1595e23b
danderle <danderle86@gmail.com>
2020-04-11 18:02:29 +0200

    The design time view models. Used only to support developement

---
+++ ProTracker/ViewModels/ControlViewModels/Design/ProjectItemControlDesignModel.cs
@@ -0,0 +1,34 @@
+﻿namespace ProTracker
+{
+    /// <summary>
+    /// A view model used for the design time data for a <see cref="ProjectItemControlViewModel"/>
+    /// </summary>
+    public class ProjectItemControlDesignModel : ProjectItemControlViewModel
+    {
+        #region Singleton
+
+        /// <summary>
+        /// A single instance of the design model
+        /// </summary>
+        public static ProjectItemControlDesignModel Instance => new ProjectItemControlDesignModel();
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public ProjectItemControlDesignModel()
+        {
+            Name = "Tetris";
+            Icon = "\uf113";
+            IconRGBbackground = "#009900";
+            Description = "Tetris game";
+            Selected = true;
+        }
+
+        #endregion
+
+    }
+}
