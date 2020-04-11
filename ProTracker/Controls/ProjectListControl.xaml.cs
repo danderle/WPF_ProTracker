8d4c8a6f1c03e7c65e1694d98c754f821806e582
danderle <danderle86@gmail.com>
2020-04-11 17:54:35 +0200

    The entire project list displayed in the side menu control

---
+++ ProTracker/Controls/ProjectListControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace ProTracker
+{
+    /// <summary>
+    /// Interaction logic for ProjectListControl.xaml
+    /// </summary>
+    public partial class ProjectListControl : UserControl
+    {
+        public ProjectListControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
