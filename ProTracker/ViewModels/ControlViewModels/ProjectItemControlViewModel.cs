08e0b33ecb0da2f27398cf632a3fae05dc5a95aa
danderle <danderle86@gmail.com>
2020-04-11 18:03:04 +0200

    The view mode for the ProjectItemControl

---
+++ ProTracker/ViewModels/ControlViewModels/ProjectItemControlViewModel.cs
@@ -0,0 +1,33 @@
+﻿namespace ProTracker
+{
+    /// <summary>
+    /// A view model for each project item in overview project list
+    /// </summary>
+    public class ProjectItemControlViewModel : BaseViewModel
+    {
+        /// <summary>
+        /// The icon used for this project
+        /// </summary>
+        public string Icon { get; set; }
+
+        /// <summary>
+        /// The RGB values for the background´color of the icon in hex
+        /// </summary>
+        public string IconRGBbackground { get; set; }
+
+        /// <summary>
+        /// The name for the project
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// A short description for the project
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// True if the project is selected
+        /// </summary>
+        public bool Selected { get; set; } = false;
+    }
+}
