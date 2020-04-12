0e4d6156ee36428a65b526f361bec86d77c5ccdf
danderle <danderle86@gmail.com>
2020-04-12 08:09:57 +0200

    added the .Core namespace

--- ProTracker/ValueConverters/ApplicationPageValueConverter.cs
+++ ProTracker/ValueConverters/ApplicationPageValueConverter.cs
@@ -1,8 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using ProTracker.Core;
+using System;
 using System.Diagnostics;
 using System.Globalization;
-using System.Text;
 
 namespace ProTracker
 {
