56cb770985bbaca15794aab6745f991c74b8425b
danderle <danderle86@gmail.com>
2020-04-12 08:10:38 +0200

    added the application view model for handeling the state of the entire application

---
+++ ProTracker.Core/ViewModels/ApplicationViewModel.cs
@@ -0,0 +1,14 @@
+﻿
+namespace ProTracker.Core.ViewModels
+{
+    /// <summary>
+    /// The application state as a view model
+    /// </summary>
+    public class ApplicationViewModel : BaseViewModel
+    {
+        /// <summary>
+        /// The current page of the application
+        /// </summary>
+        public ApplicationPage CurrentPage { get; set; } = ApplicationPage.Project;
+    }
+}
