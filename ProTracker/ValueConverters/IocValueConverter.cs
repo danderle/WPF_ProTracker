478315dad9ab6aa1139a398b57c2bf5c4df606d6
danderle <danderle86@gmail.com>
2020-04-13 09:19:38 +0200

    converts a string to a service from the IoC container

---
+++ ProTracker/ValueConverters/IocValueConverter.cs
@@ -0,0 +1,32 @@
+﻿using Ninject;
+using ProTracker.Core;
+using System;
+using System.Diagnostics;
+using System.Globalization;
+
+namespace ProTracker
+{
+    /// <summary>
+    /// Converts a string to a service pulled from the IoC container
+    /// </summary>
+    public class IocValueConverter : BaseValueConverter<IocValueConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // Find the appropriate page
+            switch ((string)value)
+            {
+                case nameof(ApplicationViewModel):
+                    return Ioc.Get<ApplicationViewModel>();
+                default:
+                    Debugger.Break();
+                    return null;
+            }
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
