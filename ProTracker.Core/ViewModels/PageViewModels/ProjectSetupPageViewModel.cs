866aa0c2999c009c5e5c44359b3df883a77f4a9c
danderle <danderle86@gmail.com>
2020-04-13 09:11:33 +0200

    added the project setup page and view model

---
+++ ProTracker.Core/ViewModels/PageViewModels/ProjectSetupPageViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System.Windows.Input;
+
+namespace ProTracker.Core
+{
+    /// <summary>
+    /// The view model for the <see cref="ProjectSetupPage.xaml"/>
+    /// </summary>
+    public class ProjectSetupPageViewModel : BaseViewModel
+    {
+        #region Private Members
+
+
+
+        #endregion
+
+        #region Public Properties
+
+
+
+        #endregion
+
+        #region Commands
+
+        /// <summary>
+        /// Command to create a new project
+        /// </summary>
+        public ICommand NewProjectCommand { get; set; }
+
+        /// <summary>
+        /// Command to continue a previous project
+        /// </summary>
+        public ICommand ContinueProjectCommand { get; set; }
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public ProjectSetupPageViewModel()
+        {
+            //Create commands
+
+        }
+
+
+        #endregion
+    }
+}
---
+++ ProTracker/Pages/ProjectSetupPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using ProTracker.Core;
+
+namespace ProTracker
+{
+    /// <summary>
+    /// Interaction logic for ProjectSetupPage.xaml
+    /// </summary>
+    public partial class ProjectSetupPage : BasePage<ProjectSetupPageViewModel>
+    {
+        public ProjectSetupPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
