eac8d5bcc333f84566114b5e8679038a268cb2b1
danderle <danderle86@gmail.com>
2020-04-13 09:17:05 +0200

    implementing the Ioc inversion of control container

--- ProTracker/App.xaml.cs
+++ ProTracker/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProTracker.Core;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +14,20 @@ namespace ProTracker
     /// </summary>
     public partial class App : Application
     {
+        /// <summary>
+        /// Custom setup which loads the Ioc immeadialty after OnStartup
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            //Setup the ioc
+            Ioc.Setup();
+
+            //Show the main window
+            Current.MainWindow = new MainWindow();
+            Current.MainWindow.Show();
+        }
     }
 }
