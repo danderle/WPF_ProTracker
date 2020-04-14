c551cc42b2562038aa68a51085e434bbc09d6bb0
danderle <danderle86@gmail.com>
2020-04-14 09:08:21 +0200

    Split the BasePage into 2 classes, one is generic and the other is not.

--- ProTracker/Pages/BasePage.cs
+++ ProTracker/Pages/BasePage.cs
@@ -9,18 +9,8 @@ namespace ProTracker
     /// <summary>
     /// A base page for all pages to gain base functionality
     /// </summary>
-    public class BasePage<VM> : Page
-        where VM : BaseViewModel, new()
+    public class BasePage : Page
     {
-        #region Private Member
-
-        /// <summary>
-        /// The View Model associated with this page
-        /// </summary>
-        private VM viewModel;
-
-        #endregion
-
         #region Public Properties
 
         /// <summary>
@@ -36,39 +26,22 @@ namespace ProTracker
         /// <summary>
         /// The time slide animations need to complete
         /// </summary>
-        public float SlideSeconds { get; set; } = 0.8f; 
+        public float SlideSeconds { get; set; } = 0.8f;
 
         /// <summary>
-        /// The View Model associated with this page
+        /// A flag indicating if this page should animate out on load
+        /// Useful for when moving a page to another frame
         /// </summary>
-        public VM ViewModel
-        {
-            get { return viewModel; }
-            set
-            {
-                // If nothing has changed, return
-                if (viewModel == value)
-                    return;
-
-                // Update the value
-                viewModel = value;
-
-                // Set the data context for this page
-                this.DataContext = viewModel;
-            }
-        }
+        public bool ShouldAnimateOut { get; set; }
 
         #endregion
 
         #region Constructor
 
-        /// <summary>
-        /// Default constructor
-        /// </summary>
         public BasePage()
         {
             //If no animation is set, dont show anything
-            if(PageLoadAnimation != PageAnimation.None)
+            if (PageLoadAnimation != PageAnimation.None)
             {
                 Visibility = Visibility.Collapsed;
             }
@@ -82,10 +55,11 @@ namespace ProTracker
             {
                 Visibility = Visibility.Visible;
             }
-
-            // Create a default view model
-            this.ViewModel = new VM();
         }
+
+        #endregion
+
+        #region Animation Load / Unload
 
         /// <summary>
         /// Listens for the page being loaded tp then do animation
@@ -94,7 +68,17 @@ namespace ProTracker
         /// <param name="e"></param>
         private async void BasePage_LoadedAsync(object sender, RoutedEventArgs e)
         {
-            await AnimateInAsync();
+            //If we are setup to animate out on load
+            if(ShouldAnimateOut)
+            {
+                //Animate out
+                await AnimateOutAsync();
+            }
+            else
+            {
+                // Animate in
+                await AnimateInAsync();
+            }
         }
 
         /// <summary>
@@ -104,12 +88,12 @@ namespace ProTracker
         public async Task AnimateInAsync()
         {
             ///Make sure that an animation is set
-            if(PageLoadAnimation == PageAnimation.None)
+            if (PageLoadAnimation == PageAnimation.None)
             {
                 return;
             }
-            
-            switch(PageLoadAnimation)
+
+            switch (PageLoadAnimation)
             {
                 case PageAnimation.SlideAndFadeInFromRight:
 
@@ -148,6 +132,62 @@ namespace ProTracker
 
             }
         }
+
+        #endregion
+    }
+    /// <summary>
+    /// A base page for all pages to gain base functionality
+    /// </summary>
+    public class BasePage<VM> : BasePage
+        where VM : BaseViewModel, new()
+    {
+        #region Private Member
+
+        /// <summary>
+        /// The View Model associated with this page
+        /// </summary>
+        private VM viewModel;
+
+        #endregion
+
+        #region Public Properties
+
+        /// <summary>
+        /// The View Model associated with this page
+        /// </summary>
+        public VM ViewModel
+        {
+            get { return viewModel; }
+            set
+            {
+                // If nothing has changed, return
+                if (viewModel == value)
+                    return;
+
+                // Update the value
+                viewModel = value;
+
+                // Set the data context for this page
+                this.DataContext = viewModel;
+            }
+        }
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public BasePage()
+        {
+            
+
+            // Create a default view model
+            this.ViewModel = new VM();
+        }
+
+        
         #endregion
     }
 }
