1b807169255d1262a40c13213c3c35394f6b2e6f
danderle <danderle86@gmail.com>
2020-04-14 09:05:49 +0200

    Attached property for disableing the default navigation bar and clearing the frame history

---
+++ ProTracker/AttachedProperties/NoFrameHistoryAttachedProperty.cs
@@ -0,0 +1,26 @@
+﻿
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ProTracker
+{
+    /// <summary>
+    /// A base class to run any animation method when a bool is set to true
+    /// and reverse animation when set to false
+    /// </summary>
+    /// <typeparam name="Parent"></typeparam>
+    public class NoFrameHistoryAttachedProperty : BaseAttachedProperty<NoFrameHistoryAttachedProperty, bool>
+    {
+        public override void OnValueUpdated(DependencyObject sender, object value)
+        {
+            var frame = sender as Frame;
+
+            frame.NavigationUIVisibility = System.Windows.Navigation.NavigationUIVisibility.Hidden;
+
+            frame.Navigated += (s, e) =>
+            {
+                (s as Frame).NavigationService.RemoveBackEntry();
+            };
+        }
+    }
+}
