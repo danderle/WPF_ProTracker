a68236bbc4fe8ba98fd1be8a3ec166bf64f265f7
danderle <danderle86@gmail.com>
2020-04-15 08:12:35 +0200

    changed the name from ProjectSetupPage to CreateProjectPage
    styled the page to have the user enter a project name and description which gets saved in a xml database

---
+++ ProTracker/Pages/CreateProjectPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using ProTracker.Core;
+
+namespace ProTracker
+{
+    /// <summary>
+    /// Interaction logic for CreateProjectPage.xaml
+    /// </summary>
+    public partial class CreateProjectPage : BasePage<CreateProjectPageViewModel>
+    {
+        public CreateProjectPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
