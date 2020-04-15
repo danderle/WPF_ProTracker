a840326801297bf2bc66c95223531521abc118dd
danderle <danderle86@gmail.com>
2020-04-15 08:10:38 +0200

    added a GoToPage function

--- ProTracker.Core/ViewModels/ApplicationViewModel.cs
+++ ProTracker.Core/ViewModels/ApplicationViewModel.cs
@@ -6,14 +6,34 @@ namespace ProTracker.Core
     /// </summary>
     public class ApplicationViewModel : BaseViewModel
     {
+        #region Public Properties
+
         /// <summary>
         /// The current page of the application
         /// </summary>
-        public ApplicationPage CurrentPage { get; set; } = ApplicationPage.Start;
+        public ApplicationPage CurrentPage { get; private set; } = ApplicationPage.Start;
 
         /// <summary>
         /// True if the side menu is visible
         /// </summary>
         public bool SideMenuControlIsVisible { get; set; } = false;
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Navigates to the specified page
+        /// </summary>
+        /// <param name="page"></param>
+        public void GoToPage(ApplicationPage page)
+        {
+            CurrentPage = page;
+
+            //Show side menu or not
+            SideMenuControlIsVisible = page == ApplicationPage.Project;
+        } 
+
+        #endregion
     }
 }
