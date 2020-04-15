d94e5b739c3dee006a3774fd714826f60fa4d058
danderle <danderle86@gmail.com>
2020-04-15 08:09:31 +0200

    A static class to handle reading and writing to the xml database

---
+++ ProTracker.Core/Models/XmlDatabase.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Linq;
+
+namespace ProTracker.Core
+{
+    /// <summary>
+    /// A static class to handle reading and writing to the xml database
+    /// </summary>
+    public static class XmlDatabase
+    {
+        #region Private Fields
+
+        /// <summary>
+        /// The path to the projects database xml file
+        /// </summary>
+        private static string databasePath = @"..\..\..\Resources\Database.xml";
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Creates a new project with the given name and description
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="description"></param>
+        public static bool CreateProject(string name, string description)
+        {
+            XElement element = XElement.Load(databasePath);
+            bool projectExists = ProjectExists(element, name);
+            if (!projectExists)
+            {
+                element.Add(
+                    new XElement("Project",
+                    new XElement("Name", name),
+                    new XElement("Description", description),
+                    new XElement("StartDate", DateTime.Now.ToString("dd/MM/yyyy")),
+                    new XElement("LastEdit", DateTime.Now.ToString("dd/MM/yyyy")),
+                    new XElement("Status", "Open"),
+                    new XElement("TotalHours", 0),
+                    new XElement("TotalDays", 1)
+                    ));
+                element.Save(databasePath);
+            }
+            return projectExists;
+        }
+
+        #endregion
+
+
+        #region Private Methods
+
+        /// <summary>
+        /// Checks if a given project name already exists
+        /// </summary>
+        /// <param name="element">the loaded xml file</param>
+        /// <param name="name">The project name to verify if it exists</param>
+        /// <returns></returns>
+        private static bool ProjectExists(XElement element, string name)
+        {
+            var projects = element.Elements();
+            foreach (var project in projects)
+            {
+                string val = project.Element("Name").Value;
+                if (val == name)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        #endregion
+    }
+}
