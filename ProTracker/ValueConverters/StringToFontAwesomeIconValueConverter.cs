71a0c70003c3f21c3f180fe0434f393f9e9dec42
danderle <danderle86@gmail.com>
2020-04-17 20:53:57 +0200

    Converts a string to font Awesome icon

---
+++ ProTracker/ValueConverters/StringToFontAwesomeIconValueConverter.cs
@@ -0,0 +1,26 @@
+﻿using Ninject;
+using ProTracker.Core;
+using System;
+using System.Diagnostics;
+using System.Globalization;
+
+namespace ProTracker
+{
+    /// <summary>
+    /// Converts a string to font Awesome icon
+    /// </summary>
+    public class StringToFontAwesomeIcon : BaseValueConverter<StringToFontAwesomeIcon>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // Find the appropriate page
+            string icon = string.Format("{0}{1}{2}", "&#x", (string)value, ";");
+            return icon;
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
