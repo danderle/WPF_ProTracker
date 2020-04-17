7029fbbded481349e4e80e1f79783f159fc57113
danderle <danderle86@gmail.com>
2020-04-17 20:44:31 +0200

    a property to handle focus  to certain element when a page is loaded

---
+++ ProTracker/AttachedProperties/TextAttachedProperties.cs
@@ -0,0 +1,25 @@
+﻿
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ProTracker
+{
+    /// <summary>
+    /// Focuses (keyboard focus) this element on load
+    /// </summary>
+    /// <typeparam name="Parent"></typeparam>
+    public class IsFocusedProperty : BaseAttachedProperty<IsFocusedProperty, bool>
+    {
+
+        public override void OnValueChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            if(!(sender is Control control))
+            {
+                return;
+            }
+
+            //Focus this control once loaded
+            control.Loaded += (s, ee) => control.Focus();
+        }
+    }
+}
