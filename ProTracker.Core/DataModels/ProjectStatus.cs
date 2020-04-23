8d75852e15547b0c6de844a196783509fd1a8db7
danderle <danderle86@gmail.com>
2020-04-23 07:23:42 +0200

    The status of a project

---
+++ ProTracker.Core/DataModels/ProjectStatus.cs
@@ -0,0 +1,28 @@
+﻿namespace ProTracker.Core
+{
+    /// <summary>
+    /// The status of a project
+    /// </summary>
+    public enum ProjectStatus
+    {
+        /// <summary>
+        /// Project in progress
+        /// </summary>
+        InProgress = 0,
+
+        /// <summary>
+        /// Project is closed
+        /// </summary>
+        CLosed,
+
+        /// <summary>
+        /// Project is delayed
+        /// </summary>
+        Delayed,
+
+        /// <summary>
+        /// Project is finished
+        /// </summary>
+        Finished,
+    }
+}
