cd9c338c04c4f00c5ef59dd21b11e5d6c4407674
danderle <danderle86@gmail.com>
2020-04-23 07:46:15 +0200

    A view model for a list of main data items

---
+++ ProTracker.Core/ViewModels/ControlViewModels/MainDataItemListViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+
+namespace ProTracker.Core
+{
+    /// <summary>
+    /// A view model for each the project list creating the overview
+    /// </summary>
+    public class MainDataItemListViewModel : BaseViewModel
+    {
+        /// <summary>
+        /// To format the <see cref="DateTimeOffset"/> use the "en-us" style format
+        /// </summary>
+        protected static string dateFormat = "dd/MM/yyyy";
+
+        /// <summary>
+        /// To format the <see cref="DateTimeOffset"/> use the "en-us" culture style
+        /// </summary>
+        protected static CultureInfo cultureInfo = new CultureInfo("en-us");
+
+        /// <summary>
+        /// The project list items for the project list
+        /// </summary>
+        public List<MainDataItemViewModel> Items { get; set; }
+    }
+}
