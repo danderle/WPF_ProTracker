58e97477615851cefccf1052d7da722527bf7fbe
danderle <danderle86@gmail.com>
2020-04-23 07:29:42 +0200

    Converts the hex string to a font awesome icon

--- ProTracker/ValueConverters/StringToFontAwesomeIconValueConverter.cs
+++ ProTracker/ValueConverters/StringToFontAwesomeIconValueConverter.cs
@@ -1,8 +1,12 @@
 ﻿using Ninject;
 using ProTracker.Core;
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Globalization;
+using System.Text;
+using System.Windows.Media;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace ProTracker
 {
@@ -13,9 +17,20 @@ namespace ProTracker
     {
         public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            // Find the appropriate page
-            string icon = string.Format("{0}{1}{2}", "&#x", (string)value, ";");
-            return icon;
+            var orig = (string)value;
+            var retVal = "Error";
+            if(orig.Length % 2 == 0)
+            {
+                var bytes = new List<byte>();
+                for (int i = 0; i < orig.Length; i += 2)
+                {
+                    byte bite = System.Convert.ToByte(orig.Substring(i, 2), 16);
+                    bytes.Add(bite);
+                }
+                bytes.Reverse();
+                retVal = Encoding.Unicode.GetString(bytes.ToArray());
+            }
+            return retVal;
         }
 
         public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
