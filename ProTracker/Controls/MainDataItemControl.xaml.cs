fba714626e8299a62f87455397f517a3e33c7db9
danderle <danderle86@gmail.com>
2020-04-23 07:50:02 +0200

    the main data control

---
+++ ProTracker/Controls/MainDataItemControl.xaml.cs
@@ -0,0 +1,17 @@
+﻿using ProTracker.Core;
+using System.Windows.Controls;
+
+namespace ProTracker
+{
+    /// <summary>
+    /// Interaction logic for MainDataItemControl.xaml
+    /// </summary>
+    public partial class MainDataItemControl : BaseUserControl
+    {
+        
+        public MainDataItemControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
