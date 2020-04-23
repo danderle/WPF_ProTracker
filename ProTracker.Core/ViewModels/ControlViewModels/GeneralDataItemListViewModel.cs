ed3f7505e6ae74c3f32269939dd3cccb121eef9b
danderle <danderle86@gmail.com>
2020-04-23 07:36:57 +0200

    View model for a list of main data items

---
+++ ProTracker.Core/ViewModels/ControlViewModels/GeneralDataItemListViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace ProTracker.Core
+{
+    /// <summary>
+    /// A view model for each the project list creating the overview
+    /// </summary>
+    public class GeneralDataItemListViewModel : BaseViewModel
+    {
+        /// <summary>
+        /// The project list items for the project list
+        /// </summary>
+        public List<ProjectItemViewModel> LoadedProjects { get; set; } = new List<ProjectItemViewModel>();
+    }
+}
