b60cb00503865d685d016240011d79fdb6c8f80a
danderle <danderle86@gmail.com>
2020-04-23 07:33:58 +0200

    design time view model for the main data of a project

---
+++ ProTracker.Core/ViewModels/ControlViewModels/Design/MainDataItemDesignModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+
+namespace ProTracker.Core
+{
+    /// <summary>
+    /// A view model used for the design time data for a <see cref="MainDataItemViewModel"/>
+    /// </summary>
+    public class MainDataItemDesignModel : MainDataItemViewModel
+    {
+        #region Singleton
+
+        /// <summary>
+        /// A single instance of the design model
+        /// </summary>
+        public static MainDataItemDesignModel Instance => new MainDataItemDesignModel();
+
+        #endregion
+
+        
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public MainDataItemDesignModel()
+        {
+            StartDate = DateTimeOffset.UtcNow.ToString(dateFormat, cultureInfo);
+            LastEdit = DateTimeOffset.UtcNow.ToString(dateFormat, cultureInfo);
+            Status = ProjectStatus.Delayed;
+            TotalDays = 10;
+            TotalHours = 4;
+            RestMinutes = 40;
+        }
+
+        #endregion
+
+    }
+}
