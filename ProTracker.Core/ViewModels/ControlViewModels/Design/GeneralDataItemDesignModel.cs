921539f92decc9076e60b6286206e2fb2162ef2a
danderle <danderle86@gmail.com>
2020-04-23 07:33:15 +0200

    design time view model for the general data of a project

---
+++ ProTracker.Core/ViewModels/ControlViewModels/Design/GeneralDataItemDesignModel.cs
@@ -0,0 +1,36 @@
+﻿namespace ProTracker.Core
+{
+    /// <summary>
+    /// A view model used for the design time data for a <see cref="GeneralDataItemViewModel"/>
+    /// </summary>
+    public class GeneralDataItemDesignModel : GeneralDataItemViewModel
+    {
+        #region Singleton
+
+        /// <summary>
+        /// A single instance of the design model
+        /// </summary>
+        public static GeneralDataItemDesignModel Instance => new GeneralDataItemDesignModel();
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public GeneralDataItemDesignModel()
+        {
+            Name = "Tetris";
+            Description = "A Tetris clone";
+            IconFont = "f113";
+            IconRgbBackground = "#009900";
+            Selected = true;
+            Editing = true;
+            DeleteMode = false;
+            Delete = false;
+        }
+
+        #endregion
+    }
+}
