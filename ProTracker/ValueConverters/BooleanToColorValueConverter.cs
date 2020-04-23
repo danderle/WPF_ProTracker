ef49a3ee4c8be08d963c007690eaaf05a9d5d875
danderle <danderle86@gmail.com>
2020-04-23 07:28:58 +0200

    fixed miss spelling in file name

--- ProTracker/ValueConverters/BooleanToColorValueConverter.cs
+++ ProTracker/ValueConverters/BooleanToColorValueConverter.cs
@@ -4,17 +4,18 @@ using System.Diagnostics;
 using System.Globalization;
 using System.Text;
 using System.Windows;
+using System.Windows.Media;
 
 namespace ProTracker
 {
     /// <summary>
     /// Converts the <see cref="Boolean"/> to an actual view/page
     /// </summary>
-    public class BooleanToVibilityValueConverter : BaseValueConverter<BooleanToVibilityValueConverter>
+    public class BooleanToColorValueConverter : BaseValueConverter<BooleanToColorValueConverter>
     {
         public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return (bool)value ? Visibility.Visible : Visibility.Collapsed;
+            return (bool)value ? new SolidColorBrush(Color.FromRgb(0xff,0x80,0)): new SolidColorBrush(Color.FromRgb(255,255,255));
         }
 
         public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
