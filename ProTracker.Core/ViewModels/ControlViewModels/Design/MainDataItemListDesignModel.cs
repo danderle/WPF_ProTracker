bc2c4e1a38aac5ee13b2fdc30359895b49e8b9aa
danderle <danderle86@gmail.com>
2020-04-23 07:34:25 +0200

    the desing time view model for the list of main data items

---
+++ ProTracker.Core/ViewModels/ControlViewModels/Design/MainDataItemListDesignModel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProTracker.Core
+{
+    /// <summary>
+    /// A view model used for the design time data for a <see cref="MainDataItemListViewModel"/>
+    /// </summary>
+    public class MainDataItemListDesignModel : MainDataItemListViewModel
+    {
+        #region Singleton
+
+        /// <summary>
+        /// A single instance of the design model
+        /// </summary>
+        public static MainDataItemListDesignModel Instance => new MainDataItemListDesignModel();
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public MainDataItemListDesignModel()
+        {
+            Items = new List<MainDataItemViewModel>
+            {
+                new MainDataItemViewModel
+                {
+                    StartDate = DateTimeOffset.UtcNow.ToString(dateFormat, cultureInfo),
+                    LastEdit = DateTimeOffset.UtcNow.ToString(dateFormat, cultureInfo),
+                    Status = ProjectStatus.Delayed,
+                    TotalDays = 10,
+                    TotalHours = 4,
+                    RestMinutes = 40,
+        },
+                new MainDataItemViewModel
+                {
+                    StartDate = DateTimeOffset.UtcNow.ToString(dateFormat, cultureInfo),
+                    LastEdit = DateTimeOffset.UtcNow.ToString(dateFormat, cultureInfo),
+                    Status = ProjectStatus.Delayed,
+                    TotalDays = 10,
+                    TotalHours = 4,
+                    RestMinutes = 40,
+                },
+                new MainDataItemViewModel
+                {
+                    StartDate = DateTimeOffset.UtcNow.ToString(dateFormat, cultureInfo),
+                    LastEdit = DateTimeOffset.UtcNow.ToString(dateFormat, cultureInfo),
+                    Status = ProjectStatus.Delayed,
+                    TotalDays = 10,
+                    TotalHours = 4,
+                    RestMinutes = 40,
+                },
+                
+            };
+        }
+
+        #endregion
+
+    }
+}
