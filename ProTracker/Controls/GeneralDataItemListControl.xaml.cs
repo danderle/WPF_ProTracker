9220bc34f7d2082a0b08e8c30f53c9706f3415fb
danderle <danderle86@gmail.com>
2020-04-23 07:49:46 +0200

    the general data list control

---
+++ ProTracker/Controls/GeneralDataItemListControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace ProTracker
+{
+    /// <summary>
+    /// Interaction logic for GeneralDataItemListControl.xaml
+    /// </summary>
+    public partial class GeneralDataItemListControl : UserControl
+    {
+        public GeneralDataItemListControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
