2eb09adc39fc4ef85c98a94b7d6c35c6f7a3b36c
danderle <danderle86@gmail.com>
2020-04-23 07:30:14 +0200

    fixed miss spelling

---
+++ ProTracker/ValueConverters/BooleanToVisibilityValueConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Text;
+using System.Windows;
+
+namespace ProTracker
+{
+    /// <summary>
+    /// Converts the <see cref="Boolean"/> to an actual view/page
+    /// </summary>
+    public class BooleanToVisibilityValueConverter : BaseValueConverter<BooleanToVisibilityValueConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool visi = (bool)value;
+            if (parameter != null)
+            {
+                visi ^= true;
+            }
+            return visi ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
