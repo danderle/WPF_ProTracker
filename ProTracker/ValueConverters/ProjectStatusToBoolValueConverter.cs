d9aec2ae56e9d201ce16f00ac4f796c015de55ff
danderle <danderle86@gmail.com>
2020-04-26 13:18:23 +0200

    added a ProjectStatusToBool value converter

---
+++ ProTracker/ValueConverters/ProjectStatusToBoolValueConverter.cs
@@ -0,0 +1,23 @@
+﻿using ProTracker.Core;
+using System;
+using System.Globalization;
+
+namespace ProTracker
+{
+    /// <summary>
+    /// Converts the <see cref="Boolean"/> to an actual view/page
+    /// </summary>
+    public class ProjectStatusToBool : BaseValueConverter<ProjectStatusToBool>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var checkboxStatus = (ProjectStatus)Enum.Parse(value.GetType(), (string)parameter);
+            return (ProjectStatus)value == checkboxStatus;
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Enum.Parse(targetType, (string)parameter);
+        }
+    }
+}
