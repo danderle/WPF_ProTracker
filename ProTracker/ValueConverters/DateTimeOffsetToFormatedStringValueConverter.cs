70f1c1009d981f4ab2d2ebf7d56309b44c25fee8
danderle <danderle86@gmail.com>
2020-04-26 12:53:55 +0200

    Added a value converter for converting a DateTimeOffset to a formated string

---
+++ ProTracker/ValueConverters/DateTimeOffsetToFormatedStringValueConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace ProTracker
+{
+    /// <summary>
+    /// Converts a string to font Awesome icon
+    /// </summary>
+    public class DateTimeOffsetToFormatedString : BaseValueConverter<DateTimeOffsetToFormatedString>
+    {
+        /// <summary>
+        /// To format the <see cref="DateTimeOffset"/> use the "en-us" style format
+        /// </summary>
+        private static string dateFormat = "dd/MM/yyyy";
+
+        /// <summary>
+        /// To format the <see cref="DateTimeOffset"/> use the "en-us" culture style
+        /// </summary>
+        private static CultureInfo cultureInfo = new CultureInfo("en-us");
+
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var dateTime = (DateTimeOffset)value;
+            return dateTime.ToString(dateFormat, culture);
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
